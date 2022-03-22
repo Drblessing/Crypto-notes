858e90f3f7ac9654a45b9cc24e7584f7513b9b0e
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2022-03-22 14:46:11 -0400

    kovan

--- Learning/Mastering Ethereum/Faucet/truffle-config.js
+++ Learning/Mastering Ethereum/Faucet/truffle-config.js
@@ -2,7 +2,7 @@ module.exports = {
   networks: {
     localnode: { // Whatever network our local node connects to
       network_id: "*", // Match any network ID
-      host: "https://kovan.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
+      host: "http/kovan.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
       port: 8545,
     }
   }
