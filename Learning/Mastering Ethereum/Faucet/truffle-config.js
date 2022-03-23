c4a584239b6194d4cea2593c0c5bc3baaaa15df4
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2022-03-23 10:07:04 -0400

    truffle debug

--- Learning/Mastering Ethereum/Faucet/truffle-config.js
+++ Learning/Mastering Ethereum/Faucet/truffle-config.js
@@ -1,9 +1,15 @@
-module.exports = {
-  networks: {
-    localnode: { // Whatever network our local node connects to
-      network_id: "*", // Match any network ID
-      host: "http/kovan.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161",
-      port: 8545,
-    }
-  }
-};
+// module.exports = {
+//   compilers: {
+//     solc: {
+//       version: "0.4.22"
+//     },
+//   }
+// };
+// module.exports = {
+//   compilers: {
+//     solc: {
+//       version: "0.4.22"
+//     },
+//   }
+// };
+
