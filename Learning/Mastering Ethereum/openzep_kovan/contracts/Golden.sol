1e2d064632c107d3bf63f54d3baec563d0f18195
Danny Blessing <52829488+Drblessing@users.noreply.github.com>
2022-04-07 13:31:07 -0400

    committ on main

--- Learning/Mastering Ethereum/openzep_kovan/contracts/Golden.sol
+++ Learning/Mastering Ethereum/openzep_kovan/contracts/Golden.sol
@@ -2,6 +2,6 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract MyCollectible is ERC721 {
-    constructor() ERC721("MyCollectible", "MCO") {}
+contract Golden is ERC721 {
+    constructor() ERC721("Godlen", "MAO") {}
 }
---
+++ Learning/Mastering Ethereum/openzep_kovan/migrations/2_deploy_contracts.js
@@ -0,0 +1,5 @@
+const Golden = artifacts.require("Golden");
+
+module.exports = function (deployer) {
+  deployer.deploy(Golden);
+};
